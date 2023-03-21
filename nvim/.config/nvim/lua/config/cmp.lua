b1aba59210b22eb57f6086334ac18212c1d5fe7f
Gregory Sieranski <git@wonbyte.com>
2023-03-21 18:34:25 -0500

    update cmp menu order

--- nvim/.config/nvim/lua/config/cmp.lua
+++ nvim/.config/nvim/lua/config/cmp.lua
@@ -67,9 +67,9 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = "nvim_lsp", keyword_length = 3 },
     { name = "buffer", keyword_length = 3 },
-  }, {
     { name = "path" },
     { name = "vsnip", filetype = "go", keyword_length = 2 },
+  }, {
     { name = "nvim_lua" },
     { name = "nvim_lsp_signature_help" },
   }),
