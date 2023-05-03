4c46a6570810e669de98293f7b814e44076f581b
Gregory Sieranski <git@wonbyte.com>
2023-05-03 13:35:09 -0500

    update diagnostic signs

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -20,7 +20,7 @@ vim.diagnostic.config({
   },
 })
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
