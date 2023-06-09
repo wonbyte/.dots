f5f6ff1e786fe447612eed50f2b731dbfb7a18fc
Greg Sieranski <git@wonbyte.com>
2023-06-09 10:24:00 -0500

    remove terraform lsp

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -124,12 +124,6 @@ lspconfig.tsserver.setup({
   handlers = handlers,
 })
 
--- Terraform
-lspconfig.terraformls.setup({
-  capabilities = capabilities,
-  handlers = handlers,
-})
-
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
