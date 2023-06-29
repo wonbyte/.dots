1871d36ee4150d2518ec0859248539512aab1534
Gregory Sieranski <git@wonbyte.com>
2023-06-29 13:53:01 -0400

    exclude build dir

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -56,4 +56,4 @@ opt.listchars = {
 }
 
 opt.wildmode = { "longest", "list", "full" }
-opt.wildignore = { "*/.git/*", "*/node_modules/*", "*/target/*" }
+opt.wildignore = { "*/.git/*", "*/node_modules/*", "*/target/*", "*/build/*" }
