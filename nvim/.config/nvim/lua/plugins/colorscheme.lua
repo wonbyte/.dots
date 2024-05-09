bdae8ef0bf52d6af65b57bad2f10d6deef318b56
Greg Sieranski <git@wonbyte.com>
2024-05-09 09:09:29 -0500

    chore(nvim): format lua

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     opts = {
-        contrast = "hard",
+      contrast = "hard",
     },
     config = function()
       vim.cmd([[colorscheme gruvbox]])
