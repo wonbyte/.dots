d7d429a7badb718afe4f0db863b11df10575d9e5
Greg Sieranski <git@wonbyte.com>
2024-05-09 09:55:59 -0500

    chore(nvim): update colorscheme setup

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,10 +4,9 @@ return {
     "ellisonleao/gruvbox.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      contrast = "hard",
-    },
+    opts = {},
     config = function()
+      require("gruvbox").setup({})
       vim.cmd([[colorscheme gruvbox]])
     end,
   },
