61243e1ee39a31e1116502ce4104606cc2b9ddab
Greg Sieranski <git@wonbyte.com>
2024-05-22 10:58:59 -0500

    feat(nvim): update for neovim 10

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -51,6 +51,8 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
