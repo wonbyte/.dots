e6f38a9101c3f486263f598a2e1e3f1c5e6cd9de
Greg Sieranski <git@wonbyte.com>
2024-06-10 12:19:21 -0400

    feat(nvim): update telescope ignore patterns

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -42,9 +42,11 @@ nnoremap("<leader>fb", require("telescope.builtin").current_buffer_fuzzy_find)
 nnoremap("<leader>ff", function()
   require("telescope.builtin").find_files({
     file_ignore_patterns = {
+      "_build",
       ".git",
-      "target",
+      "node_modules",
       "_opam",
+      "target",
       "^tmp$",
     },
   })
