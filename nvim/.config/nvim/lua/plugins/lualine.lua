73ffe2772d39494c005a19fdd9b3dd55a3388ed8
Greg Sieranski <git@wonbyte.com>
2024-07-02 10:32:08 -0500

    chore(lualine): setup bubble theme

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,42 +5,31 @@ return {
     config = function()
       require("lualine").setup({
         options = {
-          icons_enabled = true,
           theme = "auto",
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-          },
+          component_separators = "",
+          section_separators = { left = "", right = "" },
         },
         sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch" },
-          lualine_c = { "filename" },
-          lualine_x = { "encoding", "fileformat", "filetype" },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
+          lualine_a = {
+            { "mode", separator = { left = "" }, right_padding = 2 },
+          },
+          lualine_b = { "filename", "branch" },
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = { "filetype", "progress" },
+          lualine_z = {
+            { "location", separator = { right = "" }, left_padding = 2 },
+          },
         },
         inactive_sections = {
-          lualine_a = {},
+          lualine_a = { "filename" },
           lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
+          lualine_c = {},
+          lualine_x = {},
           lualine_y = {},
-          lualine_z = {},
+          lualine_z = { "location" },
         },
         tabline = {},
-        winbar = {},
-        inactive_winbar = {},
         extensions = {},
       })
     end,
