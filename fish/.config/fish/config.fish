b1825452ab44546c92117ff3afcf13d9919e2493
Greg Sieranski <git@wonbyte.com>
2024-07-23 08:45:30 -0500

    chore(fish): update opam script

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,9 @@ set -gx DOTFILES $HOME/.dots
 set -gx LANG en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
 set -gx GOPATH $HOME/.go
+# LLVM
+set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib -L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
 
 # Disable greeting
 set fish_greeting ""
@@ -13,19 +16,19 @@ fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $GOPATH/bin
 
-# Compilers to find llvm
-set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib -L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
-
-set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
-
 # Aliases
 abbr clr clear
 abbr v nvim
 abbr vi nvim
 abbr vim nvim
 
-# opam configuration
-source /Users/gps/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/gps/.opam/opam-init/init.fish' && source '/Users/gps/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
 
 # TokyoNight Color Palette
 set -l foreground c0caf5
