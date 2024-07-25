ef484819b8d4e51e1b54b09bc878133d99129e1c
Greg Sieranski <git@wonbyte.com>
2024-07-25 16:03:36 -0500

    cleanup colorscheme plugin file

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,13 @@
 return {
     {
         "catppuccin/nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        lazy = false,
+        priority = 1000,
         opts = {},
         config = function()
             require("catppuccin").setup({
                 flavour = "mocha", -- latte, frappe, macchiato, mocha
                 background = { -- :h background
-                    light = "latte",
                     dark = "mocha",
                 },
             })
