7fdd5c7e633c72da8825ee5104bb5231afe183c1
Greg Sieranski <git@wonbyte.com>
2024-07-25 16:05:05 -0500

    format colorscheme file

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,19 @@
 return {
-    {
-        "catppuccin/nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha", -- latte, frappe, macchiato, mocha
-                background = { -- :h background
-                    dark = "mocha",
-                },
-            })
-            vim.cmd([[colorscheme catppuccin]])
-        end,
-    },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("catppuccin").setup({
+        -- latte, frappe, macchiato, mocha
+        flavour = "mocha",
+        -- :h background
+        background = {
+          dark = "mocha",
+        },
+      })
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  },
 }
