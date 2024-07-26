fb2f69a70be85d58d41a7bae4c013a866ab935b2
Greg Sieranski <git@wonbyte.com>
2024-07-25 19:15:50 -0500

    set lualine theme to catppuccin

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       require("lualine").setup({
         options = {
+          theme = "catppuccin",
           component_separators = "",
           section_separators = { left = "", right = "" },
         },
