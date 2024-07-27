98c5f1a89279ccefeae328b319d0beb2664e1ad8
Greg Sieranski <git@wonbyte.com>
2024-07-26 23:45:55 -0500

    update to use the pink from catppucci (mocha)

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,22 @@ return {
     config = function()
       require("lualine").setup({
         options = {
+          theme = {
+            normal = {
+              a = { fg = '#000000', bg = '#f5c2e7', gui = 'bold' },
+              b = { fg = '#ffffff', bg = '#302d41' },
+              c = { fg = '#ffffff', bg = '#302d41' },
+            },
+            insert = { a = { fg = '#000000', bg = '#50fa7b', gui = 'bold' } },
+            visual = { a = { fg = '#000000', bg = '#ffb86c', gui = 'bold' } },
+            replace = { a = { fg = '#000000', bg = '#ff5555', gui = 'bold' } },
+            command = { a = { fg = '#000000', bg = '#8be9fd', gui = 'bold' } },
+            inactive = {
+              a = { fg = '#ffffff', bg = '#302d41', gui = 'bold' },
+              b = { fg = '#ffffff', bg = '#302d41' },
+              c = { fg = '#ffffff', bg = '#302d41' },
+            },
+          },
           component_separators = "",
           section_separators = { left = "", right = "" },
         },
