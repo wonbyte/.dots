fa29bff58fe5cb3f140a08e374bdc2e1208388f5
Greg Sieranski <git@wonbyte.com>
2024-09-09 13:33:10 -0400

    chore: remove opam

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -16,13 +16,5 @@ abbr v nvim
 abbr vi nvim
 abbr vim nvim
 
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-test -r '/Users/gps/.opam/opam-init/init.fish' && source '/Users/gps/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
-# END opam configuration
-
 # Prompt
 starship init fish | source
