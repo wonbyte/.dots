ae95ef7ac95337a5ac7f3d46d5fb879f302e8ade
Greg Sieranski <git@wonbyte.com>
2024-09-27 00:07:23 -0400

    fish: add llvm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,25 @@
-# Exports
-set -gx DOTFILES $HOME/.dots
-set -gx LANG en_US.UTF-8
-set -gx LC_ALL en_US.UTF-8
-set -gx GOPATH $HOME/.go
+# Exports (Universal Exported Variables)
+set -Ux DOTFILES $HOME/.dots
+set -Ux LANG en_US.UTF-8
+set -Ux LC_ALL en_US.UTF-8
+set -Ux GOPATH $HOME/.go
 
-# Disable greeting
-set fish_greeting ""
+# Disable greeting (Universal Variable)
+set -U fish_greeting ""
 
-# Paths
+# Paths (Universal Variable)
 set -U fish_user_paths $HOME/.local/bin $HOME/.cargo/bin $GOPATH/bin /opt/homebrew/bin
+
+# Set LLVM_PREFIX as a universal variable
+set -U LLVM_PREFIX (brew --prefix llvm)
+
+set -Ux LDFLAGS "-L$LLVM_PREFIX/lib"
+set -Ux CPPFLAGS "-I$LLVM_PREFIX/include"
+set -Ux CMAKE_PREFIX_PATH $LLVM_PREFIX
+set -Ux CC "$LLVM_PREFIX/bin/clang"
+set -Ux CXX "$LLVM_PREFIX/bin/clang++"
+
+set -U fish_user_paths $fish_user_paths $LLVM_PREFIX/bin
 
 # Aliases
 abbr clr clear
