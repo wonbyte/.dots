60ed2ac0879afad28e8d7bea4f62b59ebb76fd40
Greg Sieranski <git@wonbyte.com>
2024-10-19 21:03:51 -0500

    nvim: ocaml setup

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -103,6 +103,16 @@ return {
         },
       })
 
+      -- Ocaml
+      lspconfig.ocamllsp.setup({
+        capabilities = capabilities,
+        handlers = handlers,
+        settings = {
+          codelens = { enable = true },
+          inlayHints = { enable = true },
+        },
+      })
+
       -- Rust
       lspconfig.rust_analyzer.setup({
         settings = {
