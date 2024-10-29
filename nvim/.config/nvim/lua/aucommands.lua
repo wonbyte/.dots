a774c68185b36686e8847e03ac834532166bc3b6
Greg Sieranski <git@wonbyte.com>
2024-10-29 12:06:56 -0500

    Update aucommands.lua

--- nvim/.config/nvim/lua/aucommands.lua
+++ nvim/.config/nvim/lua/aucommands.lua
@@ -61,12 +61,3 @@ autocmd("BufWritePost", {
     vim.diagnostic.setqflist({ open = false })
   end,
 })
-
--- Properly set the file type for header files
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "*.h",
-  desc = "Detect and set the proper file type for header files",
-  callback = function()
-    vim.cmd(":set filetype=c")
-  end,
-})
