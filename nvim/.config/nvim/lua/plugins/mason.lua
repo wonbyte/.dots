00a19409e60db20a01750b2375e9d03cde2f5d75
Greg Sieranski <git@wonbyte.com>
2024-11-03 19:02:50 -0600

    nvim: ignore serverCancelled error for lsp diagnostics

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -60,6 +60,21 @@ return {
         ),
       }
 
+      -- https://github.com/neovim/neovim/issues/30985 (ISSUE)
+      -- https://github.com/neovim/neovim/pull/30999 (FIX)
+      for _, method in ipairs({
+        "textDocument/diagnostic",
+        "workspace/diagnostic",
+      }) do
+        local default_diagnostic_handler = vim.lsp.handlers[method]
+        vim.lsp.handlers[method] = function(err, result, context, config)
+          if err ~= nil and err.code == -32802 then
+            return
+          end
+          return default_diagnostic_handler(err, result, context, config)
+        end
+      end
+
       -- Set up capabilities (optional, required if using completion plugins)
       local capabilities = vim.lsp.protocol.make_client_capabilities()
 
