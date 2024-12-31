d894f217f2b5507bdb12802874457d70ba3b23e0
Greg Sieranski <git@wonbyte.com>
2024-12-30 23:46:27 -0500

    Update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,7 +2,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazyrepo,
+    lazypath,
+  })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -17,16 +24,9 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Set <Space> as the leader key (must be set before plugins are loaded)
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.maplocalleader = "\\"
 
--- Load Custom Settings
 require("settings")
-
--- Load Autocommands
 require("aucommands")
-
--- Setup lazy.nvim
-require("lazy").setup("plugins")
-
--- Load Key Mappings (needs "plugins")
 require("keymappings")
+require("lazy").setup("plugins")
